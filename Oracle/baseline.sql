6d4ba4f3b185b3a590d18db7c3689346458ce4c1
Song <xsm7f@umsystem.edu>
2021-09-29 10:34:31 -0500

    add placeholder for local lab codes

--- Oracle/baseline.sql
+++ Oracle/baseline.sql
@@ -13,7 +13,7 @@
   - DISPENSING
 */
 
-/*baseline demographic table: 1 patient-provider per row*/
+/*baseline demographic table: 1 patient-payer per row*/
 create table BL_DEMO as 
 with demo_payor_rank as (
 select p.PATID
@@ -24,12 +24,12 @@ select p.PATID
       ,d.HISPANIC
       ,d.PAT_PREF_LANGUAGE_SPOKEN 
       ,a.ADDRESS_ZIP9
-      ,ds.RAW_PAYOR_NAME
+      ,e.RAW_PAYER_NAME_PRIMARY
       ,dense_rank() over (partition by p.PATID order by case when p.INDEX_DATE between ds.MEM_EFF_FROM_DATE and coalesce(ds.MEM_EFF_TO_DATE, CURRENT_DATE) then 0 else 1 end) rn 
 from pat_incld p
 join DEMOGRPHIC d on p.PATID = d.PATID
 left join &&PCORNET_CDM_SCHEMA.PRIVATE_ADDRESS_HISTORY a on p.PATID = a.PATID
-left join DEMOGRAPHIC_SUPP ds on p.PATID = ds.PATID 
+left join ENCOUNTER e on p.PATID = e.PATID 
 )
 select PATID
       ,BIRTH_DATE
@@ -44,10 +44,14 @@ from demo_payor_rank
 where rn = 1
 ;
 
+--check for duplicates
+
+
+
 /*baseline lab table: 1 patient-lab-observation per row*/
 create table BL_LAB as
 -- Total cholesterol 
-select p.PATID
+select distinct p.PATID
       ,NVL(l.SPECIMEN_DATE,l.LAB_ORDER_DATE) as LAB_DATE 
       ,'TC' as LAB_NAME
       ,l.RESULT_NUM
@@ -55,18 +59,24 @@ select p.PATID
 from pat_incld p 
 join LAB_RESULT_CM l on l.PATID = p.PATID
 where ( 
-       /*--LOINC code set*/
-       l.LAB_LOINC in ( 
-          '2093-3'
-         ,'48620-9'
-         ,'35200-5'
-         ,'14647-2'
-         )
-       and UPPER(l.RESULT_UNIT) = 'MG/DL'
+       (/*--LOINC code set*/
+        l.LAB_LOINC in ( 
+           '2093-3'
+          ,'48620-9'
+          ,'35200-5'
+          ,'14647-2'
+          ) 
+--       or
+--        (/*--local code set*/
+--         l.RAW_LAB_CODE in (
+--           'KUH|COMPONENT_ID:'    
+--         )
+        )
+       and UPPER(l.RESULT_UNIT) = 'MG/DL' -- may want to comment it out to avoid missing records if the measuring units hasn't been standardized
       ) 
 union all
 -- LDL
-select p.PATID
+select distinct p.PATID
       ,NVL(l.SPECIMEN_DATE,l.LAB_ORDER_DATE) as LAB_DATE 
       ,'LDL-C' as LAB_NAME
       ,l.RESULT_NUM
@@ -74,26 +84,32 @@ select p.PATID
 from pat_incld p 
 join LAB_RESULT_CM l on l.PATID = p.PATID
 where ( 
-       /*--LOINC code set*/
-       l.LAB_LOINC in ( 
-          '2089-1'
-         ,'18262-6'
-         ,'49132-4'
-         ,'35198-1'
-         ,'39469-2'
-         ,'12773-8'
-         ,'18261-8'
-         ,'22748-8'
-         ,'13457-7'
-         ,'9346-8'
-         ,'2574-2'
-         ,'14815-5'
-         )
-       and UPPER(l.RESULT_UNIT) = 'MG/DL'
+       (/*--LOINC code set*/
+        l.LAB_LOINC in ( 
+           '2089-1'
+          ,'18262-6'
+          ,'49132-4'
+          ,'35198-1'
+          ,'39469-2'
+          ,'12773-8'
+          ,'18261-8'
+          ,'22748-8'
+          ,'13457-7'
+          ,'9346-8'
+          ,'2574-2'
+          ,'14815-5'
+          ) 
+--          or
+--        (/*--local code set*/
+--         l.RAW_LAB_CODE in (
+--           'KUH|COMPONENT_ID:'    
+--         )
+        )
+       and UPPER(l.RESULT_UNIT) = 'MG/DL' -- may want to comment it out to avoid missing records if the measuring units hasn't been standardized
       )
 union all
 -- HDL
-select p.PATID
+select distinct p.PATID
       ,NVL(l.SPECIMEN_DATE,l.LAB_ORDER_DATE) as LAB_DATE 
       ,'HDL-C' as LAB_NAME
       ,l.RESULT_NUM
@@ -101,22 +117,28 @@ select p.PATID
 from pat_incld p 
 join LAB_RESULT_CM l on l.PATID = p.PATID
 where ( 
-       /*--LOINC code set*/
-       l.LAB_LOINC in ( 
-          '2085-9'
-         ,'49130-8'
-         ,'35197-3'
-         ,'12771-2'
-         ,'12772-0'
-         ,'18263-4'
-         ,'27340-9'
-         ,'14646-4'
-         )
-       and UPPER(l.RESULT_UNIT) = 'MG/DL'
+       (/*--LOINC code set*/
+        l.LAB_LOINC in ( 
+           '2085-9'
+          ,'49130-8'
+          ,'35197-3'
+          ,'12771-2'
+          ,'12772-0'
+          ,'18263-4'
+          ,'27340-9'
+          ,'14646-4'
+          )
+--       or
+--        (/*--local code set*/
+--         l.RAW_LAB_CODE in (
+--           'KUH|COMPONENT_ID:'    
+--         )
+        )
+       and UPPER(l.RESULT_UNIT) = 'MG/DL' -- may want to comment it out to avoid missing records if the measuring units hasn't been standardized
       )
 union all
 -- Triglycerides
-select p.PATID
+select distinct p.PATID
       ,NVL(l.SPECIMEN_DATE,l.LAB_ORDER_DATE) as LAB_DATE 
       ,'TG' as LAB_NAME
       ,l.RESULT_NUM
@@ -124,17 +146,23 @@ select p.PATID
 from pat_incld p 
 join LAB_RESULT_CM l on l.PATID = p.PATID
 where ( 
-       /*--LOINC code set*/
-       l.LAB_LOINC in ( 
-          '2571-8'
-         ,'1644-4'
-         ,'3043-7'
+       (/*--LOINC code set*/
+        l.LAB_LOINC in ( 
+           '2571-8'
+          ,'1644-4'
+          ,'3043-7'
          )
-       and UPPER(l.RESULT_UNIT) = 'MG/DL'
+--       or
+--        (/*--local code set*/
+--         l.RAW_LAB_CODE in (
+--           'KUH|COMPONENT_ID:'    
+--         ) 
+       )        
+       and UPPER(l.RESULT_UNIT) = 'MG/DL' -- may want to comment it out to avoid missing records if the measuring units hasn't been standardized
       )
 union all
 -- Serum Creatinine
-select p.PATID
+select distinct p.PATID
       ,NVL(l.SPECIMEN_DATE,l.LAB_ORDER_DATE) as LAB_DATE 
       ,'SCr' as LAB_NAME
       ,l.RESULT_NUM
@@ -142,23 +170,29 @@ select p.PATID
 from pat_incld p 
 join LAB_RESULT_CM l on l.PATID = p.PATID
 where ( 
-       /*--LOINC code set*/
-       l.LAB_LOINC in ( 
-           '2160-0'
-          ,'38483-4'
-          ,'14682-9'
-          ,'21232-4'
-          ,'35203-9'
-          ,'44784-7'
-          ,'59826-8'
-         )
+       (/*--LOINC code set*/
+        l.LAB_LOINC in ( 
+            '2160-0'
+           ,'38483-4'
+           ,'14682-9'
+           ,'21232-4'
+           ,'35203-9'
+           ,'44784-7'
+           ,'59826-8'
+          )
+--       or
+--        (/*--local code set*/
+--         l.RAW_LAB_CODE in (
+--           'KUH|COMPONENT_ID:'    
+--         )
+       )
        and (UPPER(l.RESULT_UNIT) = 'MG/DL' or UPPER(l.RESULT_UNIT) = 'MG') /*there are variations of common units*/
        and l.SPECIMEN_SOURCE <> 'URINE' /*only serum creatinine*/
        and l.RESULT_NUM > 0 /*value 0 could exist*/
       )
 union all
 -- Urine Protein Creatinine ratio
-select p.PATID
+select distinct p.PATID
       ,NVL(l.SPECIMEN_DATE,l.LAB_ORDER_DATE) as LAB_DATE 
       ,'UProtCrRatio' as LAB_NAME
       ,l.RESULT_NUM
@@ -166,18 +200,25 @@ select p.PATID
 from pat_incld p 
 join LAB_RESULT_CM l on l.PATID = p.PATID
 where ( 
-       /*--LOINC code set*/
-       l.LAB_LOINC in ( 
-          '34366-5'
-         ,'40486-3'
-         ,'14959-1'
-         ,'2889-4'
+       (/*--LOINC code set*/
+        l.LAB_LOINC in ( 
+           '34366-5'
+          ,'40486-3'
+          ,'14959-1'
+          ,'2889-4'
          )
+--       or
+--        (/*--local code set*/
+--         l.RAW_LAB_CODE in (
+--           'KUH|COMPONENT_ID:'    
+--         )
       )
+   )
 ;
 
 /*baseline medical history/condition table: 1 patient-condition per row*/
 create table BL_COND as
+with event_deck as (
 -- history of coronary artery disease
 select  dx.PATID
       ,'DX' as IDENTIFIER_TYPE
@@ -200,7 +241,7 @@ where (dx.DX_TYPE = '10' and
         dx.DX like 'V45.81%' or 
         dx.DX like 'V45.82%'))
 union all
--- Coronary revascularization
+-- history of coronary artery disease: coronary revascularization
 select px.PATID
       ,'PX' as IDENTIFIER_TYPE
       ,'CAD' as CONDITION
@@ -271,19 +312,25 @@ select dx.PATID
 from pat_incld p
 join LAB_RESULT_CM lab on p.PATID = lab.PATID
 where ( 
-       /*--LOINC code set*/
-       l.LAB_LOINC in ( 
-          '33914-3'
-         ,'50044-7'
-         ,'48642-3'
-         ,'48643-1'
-         ,'62238-1'
-         ,'88293-6'
-         ,'88294-4'
-         ,'69405-9'
-         ,'94677-2'
-         ) and
-       lab.RESULT_NUM < 60
+       (/*--LOINC code set for eGFR/GFR*/
+        l.LAB_LOINC in ( 
+           '33914-3'
+          ,'50044-7'
+          ,'48642-3'
+          ,'48643-1'
+          ,'62238-1'
+          ,'88293-6'
+          ,'88294-4'
+          ,'69405-9'
+          ,'94677-2'
+         )
+--       or
+--        (/*--local code set*/
+--         l.RAW_LAB_CODE in (
+--           'KUH|COMPONENT_ID:'    
+--         ) 
+       )
+      and lab.RESULT_NUM < 60
       )
 union all
 -- history of dyslipidemia
@@ -299,6 +346,12 @@ where (dx.DX_TYPE = '10' and
       or 
       (dx.DX_TYPE = '09' and
        (dx.DX like '272.4%'))
+)
+select distinct PATID
+      ,CONDITION
+      ,CONDITIOn_DATE
+      ,DAYS_SINCE_ENROLL
+from event_deck
 ;
 
 /*stage the supplement concept_set file ConceptSet_Med_AntiHTN.csv
