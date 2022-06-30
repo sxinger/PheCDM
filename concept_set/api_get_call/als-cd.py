0017964a7ee579c6a6135aa1738ebc4bfd83f529
Song <xsm7f@umsystem.edu>
2022-06-30 02:18:35 -0500

    change folder name

--- concept_set/api_get_call/als-cd.py
+++ concept_set/api_get_call/als-cd.py
@@ -8,18 +8,18 @@ api_key=bps.get_access_info("./.config/config.json")['bioportal']['api_key']
 # generate a bps batch write instance
 # bps.batch_write_vs_excel(
 #      api_key
-#     ,'C:/repo/PheCDM/concept_set/autogen_set'
+#     ,'C:/repo/PheCDM/concept_set/valueset_autogen'
 #     ,'als-endpoint_input'
 #     )
 
 # bps.batch_write_vs_excel(
 #      api_key
-#     ,'C:/repo/PheCDM/concept_set/autogen_set'
+#     ,'C:/repo/PheCDM/concept_set/valueset_autogen'
 #     ,'als-diagnostic_input'
 #     )
 
 bps.batch_write_vs_json(
      api_key
-    ,'C:/repo/PheCDM/concept_set/autogen_set'
+    ,'C:/repo/PheCDM/concept_set/valueset_autogen'
     ,'als-diagnostic_input'
     )
