1fdc9ee1b6abacaaf994cf7bce7b6dc324fd7ce0
Song <xsm7f@umsystem.edu>
2022-07-08 14:06:54 -0500

    add deduplication process after loading ecqm valuesets

--- concept_set/proc_download/ecqi_proc_download.py
+++ concept_set/proc_download/ecqi_proc_download.py
@@ -9,10 +9,11 @@ import json
 
 path_to_download = 'C:/Users/xsm7f/Downloads'
 file_name = "ec_eh_oqr_category_20220505"
+# file_name = "test"
 
-if not exists(f'{path_to_download}/{file_name}.zip'):
+if exists(f'{path_to_download}/{file_name}.zip') and not exists(f'{path_to_download}/{file_name}.xlsx'):
     # unzip
-    zipped_file = ZipFile(f'{path_to_download}/{file_name}')
+    zipped_file = ZipFile(f'{path_to_download}/{file_name}.zip')
     zipped_file.extractall(path_to_download)
     file_lst = zipped_file.namelist()
     print(f'files downloaded:{file_lst} ')
@@ -34,7 +35,8 @@ for key, df in multisheet.items():
                                 "excld":r[vs_col[4]]}
         # gather codelist included in the value set
         sub_df = df[df[vs_col[0]]==r[vs_col[0]]]
-        sub_df_dict = sub_df.groupby(['Code System']).apply(lambda x: list(zip(x['Code'].tolist(),x['Description'].tolist()))).to_dict()
+        sub_df_zip = sub_df.groupby('Code System').apply(lambda x: list(zip(x['Code'].to_list(),x['Description'].to_list()))).to_dict()
+        sub_df_dict = {k: list(set(v)) for k,v in sub_df_zip.items()} # not sure why last step created duplications
         ecqm_set[r[vs_col[0]]]["codelist"] = sub_df_dict
     
     # dump dict as json file
